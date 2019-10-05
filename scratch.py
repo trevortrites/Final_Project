a045dc69c31ead517bc18fbced310b9b109b852c
trevortrites <55851178+trevortrites@users.noreply.github.com>
2019-10-05 12:48:31 -0400

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,30 @@
+### print("Type 1 for Color to Hex. Type 2 for Hex to a Color")
+### input()
+## if input == "1":
+print("What color would you like to convert to Hex?")
+color = input()
+if color == 'Black':
+  print('#000000')
+
+
+# Black
+# (0,0,0)
+#000000
+
+# White
+# (255,255,255)
+#FFFFFF
+
+# Red
+# (255,0,0)
+#FF0000
+
+# Lime
+# (0,255,0)
+#00FF00
+
+# Blue
+# (0,0,255)
+#00FF00
+
+
