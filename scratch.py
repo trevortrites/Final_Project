3366bdef0ee9f44c0452c809d66ef1d46b3a25e9
trevortrites <55851178+trevortrites@users.noreply.github.com>
2019-10-12 10:55:40 -0400

    Add files via upload

--- scratch.py
+++ scratch.py
@@ -7,24 +7,88 @@ if color == 'Black':
   print('#000000')
 
 
-# Black
-# (0,0,0)
-#000000
 
-# White
-# (255,255,255)
-#FFFFFF
+# Colors that will be used
 
-# Red
-# (255,0,0)
-#FF0000
+            # Black
+                  # (0,0,0)
+                  #000000
+Black = {'RGB':'(0,0,0)','Hex':'#000000'}
 
-# Lime
-# (0,255,0)
-#00FF00
+            # White
+                  # (255,255,255)
+                  #FFFFFF
+White = {'RGB':'(255,255,255)','Hex':'#FFFFFF'}
 
-# Blue
-# (0,0,255)
-#00FF00
+            # Red
+                  # (255,0,0)
+                  #FF0000
+Red = {'RGB':'(255,0,0)','Hex':'FF0000'}
 
+            # Lime
+                  # (0,255,0)
+                  #00FF00
+Lime = {'RGB':'(0,255,0)','Hex':'00FF00'}
+
+            # Blue
+                  # (0,0,255)
+                  #00FF00
+Blue = {'RGB':'(0,0,255)','Hex':'#00FF00'}
+
+            # Yellow
+                  # (255,255,0)
+                  #FFFF00
+Yellow = {'RGB':'(255,255,0)','Hex':'FFFF00'}
+
+            # Cyan
+                  # (0,255,255)
+                  #00FFFF
+Cyan = {'RGB':'(0,255,255)','Hex':'#00FFFF'}
+
+            # Magenta
+                  # (255,0,255)
+                  #FF00FF
+Magenta = {'RGB':'(255,0,255)','Hex':'#FF00FF'}
+
+            # Silver
+                  # (192,192,192)
+                  #C0C0C0
+Silver = {'RGB':'(255,0,255)','Hex':'#C0C0C0'}
+
+            # Gray
+                  # (128,128,128)
+                  #808080
+Gray = {'RGB':'(128,128,128)','Hex':'#808080'}
+
+            # Maroon
+                  # (128,0,0)
+                  #800000
+Maroon = {'RGB':'(128,0,0)','Hex':'#800000'}
+
+            # Olive
+                  # (128,128,0)
+                  #808000
+Olive = {'RGB':'(128,128,0','Hex':'#808000'}
+
+            # Green
+                  # (0,128,0)
+                  #008000
+Green = {'RGB':'(128,128,0)','Hex':'#008000'}
+
+            # Purple
+                  # (128,0,128)
+                  #800080
+Purple = {'RGB':'(128,128,0)','Hex':'#800080'}
+
+            # Teal
+                  # (0,128,128)
+                  #008080
+Teal = {'RGB':'(128,128,0','Hex':'#008080'}
+
+            # Navy
+                  # (0,0,128)
+                  #000080
+Navy = {'RGB':'(0,0,128)','Hex':'#000080'}
+
+Colors = [Black, White, Red, Lime, Blue, Yellow, Cyan, Magenta, Silver, Gray, Maroon, Olive, Green, Purple, Teal, Navy]
 
