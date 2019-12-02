e337832655fa0f1f4158ae982ed9371f9ca49db8
trevortrites <55851178+trevortrites@users.noreply.github.com>
2019-12-02 12:07:35 -0500

    Add files via upload

--- scratch.py
+++ scratch.py
@@ -1,107 +1,59 @@
+from time import sleep
+
 def rgb2hex(r,g,b,):
   hex = "#{:02x}{:02x}{:02x}".format(r,g,b)
   return hex
 
-r = int(input("Enter r: "))
-g = int(input("Enter g: "))
-b = int(input("Enter b: "))
-rgb = (r,g,b)
-hex = rgb2hex(rgb[0],rgb[1],rgb[2])
-print(hex)
+def hex2rgb(hex):
+
+
+  #rgb = tuple(map(ord,hex[1:].decode('hex')))
+  #return rgb
+
+
+def prompt():
+  print("Hello and welcome to my color converter!")
+  sleep(2)
+  while True:
+    option = input("To convert Hex to RGB press '1', press '2' for RGB to Hex, or press '0' to exit.")
+    option = option.upper()
+
+    if option == '1':
+      print("Hex to RGB..")
+      sleep(1)
+      hex = input("Please enter 6 digits: ")
+      rgb = hex2rgb(hex)
+      print(rgb)
+
+    elif option == '2':
+      print("RGB to Hex..")
+      sleep(1)
+      r = int(input("Enter red value: "))
+      if r > 255 or r < 0:
+        print ("ERROR - Not within 0-255 range")
+        return
+      g = int(input("Enter green value: "))
+      if g > 255 or g < 0:
+        print ("ERROR - Not within 0-255 range")
+        return
+      b = int(input("Enter blue value: "))
+      if b > 255 or b < 0:
+        print ("ERROR - Not within 0-255 range")
+        return
+      rgb = (r,g,b)
+      hex = "#{:02x}{:02x}{:02x}".format(r,g,b)
+      print(hex)
+
+    elif option == '0':
+      print("Thanks for converting!")
+      sleep(1)
+      exit()
+
+    else:
+      print("ERROR")
+
+prompt()
 
 
 
-### print("Type 1 for Color to Hex. Type 2 for Hex to a Color")
-### input()
-## if input == "1":
-#print("What color would you like to convert to Hex?")
-#color = input()
-#if color == 'Black':
-  #print('#000000')
-
-
-
-# Colors that will be used
-
-            # Black
-                  # (0,0,0)
-                  #000000
-Black = {'RGB':'(0,0,0)','Hex':'#000000'}
-
-            # White
-                  # (255,255,255)
-                  #FFFFFF
-White = {'RGB':'(255,255,255)','Hex':'#FFFFFF'}
-
-            # Red
-                  # (255,0,0)
-                  #FF0000
-Red = {'RGB':'(255,0,0)','Hex':'FF0000'}
-
-            # Lime
-                  # (0,255,0)
-                  #00FF00
-Lime = {'RGB':'(0,255,0)','Hex':'00FF00'}
-
-            # Blue
-                  # (0,0,255)
-                  #00FF00
-Blue = {'RGB':'(0,0,255)','Hex':'#00FF00'}
-
-            # Yellow
-                  # (255,255,0)
-                  #FFFF00
-Yellow = {'RGB':'(255,255,0)','Hex':'FFFF00'}
-
-            # Cyan
-                  # (0,255,255)
-                  #00FFFF
-Cyan = {'RGB':'(0,255,255)','Hex':'#00FFFF'}
-
-            # Magenta
-                  # (255,0,255)
-                  #FF00FF
-Magenta = {'RGB':'(255,0,255)','Hex':'#FF00FF'}
-
-            # Silver
-                  # (192,192,192)
-                  #C0C0C0
-Silver = {'RGB':'(255,0,255)','Hex':'#C0C0C0'}
-
-            # Gray
-                  # (128,128,128)
-                  #808080
-Gray = {'RGB':'(128,128,128)','Hex':'#808080'}
-
-            # Maroon
-                  # (128,0,0)
-                  #800000
-Maroon = {'RGB':'(128,0,0)','Hex':'#800000'}
-
-            # Olive
-                  # (128,128,0)
-                  #808000
-Olive = {'RGB':'(128,128,0','Hex':'#808000'}
-
-            # Green
-                  # (0,128,0)
-                  #008000
-Green = {'RGB':'(128,128,0)','Hex':'#008000'}
-
-            # Purple
-                  # (128,0,128)
-                  #800080
-Purple = {'RGB':'(128,128,0)','Hex':'#800080'}
-
-            # Teal
-                  # (0,128,128)
-                  #008080
-Teal = {'RGB':'(128,128,0','Hex':'#008080'}
-
-            # Navy
-                  # (0,0,128)
-                  #000080
-Navy = {'RGB':'(0,0,128)','Hex':'#000080'}
-
-Colors = [Black, White, Red, Lime, Blue, Yellow, Cyan, Magenta, Silver, Gray, Maroon, Olive, Green, Purple, Teal, Navy]
 
